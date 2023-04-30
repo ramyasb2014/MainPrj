a3cdef364d148d3974eace7c2843137e783d84dd
ramyasb2014 <ramyasb2014@gmail.com>
2023-04-30 13:56:35 +0530

    New Project Created

---
+++ src/test/java/com/tec/ClassB.java
@@ -0,0 +1,16 @@
+package com.tec;
+
+public class ClassB {
+	private void method1() {
+		System.out.println("A");
+	}
+	private void method2() {
+		System.out.println("B");
+
+	}
+	private void method3() {
+		System.out.println("C");
+
+	}
+
+}
